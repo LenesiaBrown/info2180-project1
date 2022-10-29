0653c2ca887a2baf0e438ba7e161b17780e32c9d
Lenesia Brown <lenesiabrown37@gmail.com>
2022-10-29 14:39:24 -0500

    Javascript added

--- info2180-project1-starter-master/scripts.js
+++ info2180-project1-starter-master/scripts.js
@@ -1,1 +1,30 @@
-/* Add your JavaScript to this file */
+window.onload = function() {
+    const email1 = document.getElementById("email");
+    const me = document.body.getElementsByClassName("message")[0];
+    var t = document.body.getElementsByTagName("form")[0];
+    t.addEventListener("submit", function(event) {
+        let msg = [];
+        console.log(email1.value);
+        if (email1.value === "" || email1.value == null || 
+        !email1.value === /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ ) {
+            msg.push("Please enter a valid email address.");
+            
+        } else {
+            msg.push("Thank you! Your email address" + " " + email1.value + " " +
+            "has been added to our mailing list!");
+        }
+
+        if (msg.length > 0) {
+            event.preventDefault();
+            me.innerText = msg.join(", ");            
+        } else {
+            event.preventDefault();
+            me.innerText = msg.join(", ");
+        }
+        
+    });
+    
+}
+
+
+
